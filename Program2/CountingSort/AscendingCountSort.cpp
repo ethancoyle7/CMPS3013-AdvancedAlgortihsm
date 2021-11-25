1afa62d692f6d730b6919d89d5d0e6a35272d5a9
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-11-25 00:11:59 -0600

    Update AscendingCountSort.cpp

--- Program2/CountingSort/AscendingCountSort.cpp
+++ Program2/CountingSort/AscendingCountSort.cpp
@@ -13,15 +13,15 @@
 //===============================================================================//
 // Instructor : Dr. Colmenares                                                   //
 // Class      : CMPS 3013 Advanced Algorithms                                    //
-// Assignment : Program 4 Selection Sort with (AscendingOrder.txt)               //
+// Assignment : Program 4 Counting Sort  with (AscendingOrder.txt)               //
 //===============================================================================//
 // What it does -                                                                //
-//    This programs purpose isto utilize selection sort                          //
+//    This programs purpose isto utiliz counting  sort                           //
 //    on the infile with specified   order                                       //
 //    numbers 0 to 100000 and will catch the time                                //
 //    that it takes for the program to run and                                   //
 //    perform the sorting of the infile using                                    // 
-//    the selection sorting on the infile contain                                //
+//    the counting sorting  on the infile contain                                //
 //    the ascending order elements from 0- 100000                                //
 //    amd then output the time in a nice, easy to                                //
 //    read format for the viewer to see how long                                 //
@@ -29,25 +29,23 @@
 //    ammount of time it takes to sort the given                                 //
 //    input that is placed inside of an array                                    //
 //===============================================================================//
-
 //===============================================================================//
 //    sorting code attributes by :                                               //
 //                                                                               //
-// https://www.tutorialspoint.com/cplusplus-program-to-implement-selection-sort  // 
+// https://www.bogotobogo.com/Algorithms/countingsort.php                        // 
 //===============================================================================//
 
 #include<iostream>
 #include <fstream>
 #include <chrono>    //for the start and stop time
-#include <time.h>    //clock, clock_t
-using namespace std::chrono;
+#include <time.h>    //clock
+using namespace std::chrono;// timer 
 using namespace std;
 
-// function prototypes
+//function prototypes
 void openFiles(ifstream& InFile, ofstream& OutFile);
-void selectionSort(int* array, int size);
-void swapping(int& a, int& b);
-void display(int* array, int, ofstream& OutFile);
+void print(int a[], int sz, ofstream& OutFile);
+
 
 //#####################################################//
 //f(x) name                                            // 
@@ -80,37 +78,9 @@ void openFiles(ifstream& InFile, ofstream& OutFile)
     OutFile.open(outFileName);
 }
 
-// for isplaying purposes for reader and viewer easement
 //*********************************************************************//
 //* Function Name:												       //
-//*	->void swapping(int &a, int &b)                                    //
-//*    -> prototype at the top                                         //
-//																	   //
-//* What does it do?												   //
-//	  -> The purpose of this function is that when it is called when   //
-//    -> parsing through the array, if there is a min value, to swap   //
-//    -> the values in the compared element by pointing to the address //
-//    -> and accessing the elemnt inside of those comparisons          //
-//						    	  	                                   //
-//* Parameters:	    						                       	   //
-//*  ->   This function takes two parameters: address to the first     //
-//*  ->   element and the address to second element so can swap        //
-//*																       //
-//* Returns:														   //
-//*   -> Void( the variables are being passed by reference so no return//   
-//*********************************************************************//
-void swapping(int& a, int& b)
-{
-    //swap the content of a and b
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-}
-// for isplaying purposes for reader and viewer easement
-//*********************************************************************//
-//* Function Name:												       //
-//*	->void display(int *array, int size)                               //
+//*	->void print(int a[], int sz,ofstream& OutFile)                    //
 //*    -> prototype at the top                                         //
 //																	   //
 //* What does it do?												   //
@@ -125,94 +95,109 @@ void swapping(int& a, int& b)
 //* Returns:														   //
 //*   -> Void( the variables are being passed by reference so no return//   
 //*********************************************************************//
-void display(int* array, int size, ofstream& OutFile)
+void print(int a[], int sz,ofstream& OutFile) 
 {
-    OutFile << "The Sorted Array is Disaplyed Below\n\n";
-    OutFile << "====================================\n\n";
-    for (int i = 0; i < size; i++)
-        OutFile << array[i] << " " << endl;
+	OutFile << "\nThe Sorted Array is Disaplyed Below\n\n";
+	OutFile << "====================================\n\n";
+	for (int i = 0; i < sz; i++) 
+		OutFile << a[i] << " "<< endl;
 }
 
 //*********************************************************************//
 //* Function Name:												       //
-//*	->void selectionSort(int *array, int size)                         //
-//*    -> prototype at the top                                         //
+//*	->void CountingSort(int arr[], int sz)                             //
 //																	   //
 //* What does it do?												   //
-//	  -> The purpose of this function is to compare values in the array//
-//	  -> with a pointer to the elements for the whole size of the array//
-//    -> and then compare for min value and if there is a min in compar//
-//	  -> swap the values and call the swap function					   //
+//	  -> The purpose of this function is to parse through the read in  //
+//    -> Data and then perform the sorting algorithm on the data inside//
+//    -> the array until it is fully sorted                            //
 //						    	  	                                   //
 //* Parameters:	    						                       	   //
-//*  ->   This takes a integer pointer to an array and the size of arra//
-//*   ->   as its parameters                                           //
+//*  ->   This function takes two parameters, the integer array and the//
+//   -> counter which will both be used inside of the sorting algorithm//
 //*																       //
 //* Returns:														   //
-//*   -> Void( the variables are being passed by reference so no return//   
+//*   ->There is no return because this function is void. However, This//
+//*   -> finishes going through the whole array until there is no      //
+//*   -> comparisons left to make                                      //   
 //*********************************************************************//
-void selectionSort(int* array, int size)
+void CountingSort(int arr[], int sz) 
 {
-    int i, j, imin;
-    for (i = 0; i < size - 1; i++) {
-        imin = i;   //get index of minimum data
-        for (j = i + 1; j < size; j++)
-            if (array[j] < array[imin])
-                imin = j;
-        //placing in correct position
-        swap(array[i], array[imin]);
-    }
+	//initilize the variables
+	int i, j, k;
+	int idx = 0;
+	int min, max;
+	//min and max both equal to the first index
+	min = max = arr[0];
+	//loop through to find the min and max
+	for (i = 1; i < sz; i++) 
+	{
+		min = (arr[i] < min) ? arr[i] : min;
+		max = (arr[i] > max) ? arr[i] : max;
+	}
+
+	k = max - min + 1;
+	/* creates k buckets */
+	int* B = new int[k];
+	for (i = 0; i < k; i++) B[i] = 0;
+
+	//sorting the array
+	for (i = 0; i < sz; i++) B[arr[i] - min]++;
+	for (i = min; i <= max; i++)
+		for (j = 0; j < B[i - min]; j++) arr[idx++] = i;
+	delete[] B;// free up the dynamically allocated memory
 }
 
-
-
-
+// main driver of the program where all the calls and timing 
+//will be made
 int main()
 {
-    srand(time(NULL)); //set time to null
-    // size of the infile is 100001
-    int size = 100001;
-    // create te container array to hold the values
-    int* Container = new int[size];
-    // start tge counter for the index and the element
-    int count = 0;
-    int n = 0;
+	// create the in and out files 
+	//go to the function to read in both files
+	ifstream InFile;
+	ofstream OutFile;
+	openFiles(InFile, OutFile);// prompt for input output
+	
+	// size of the infile is 100001
+	int size = 100001;
+	// create te container array to hold the values
+	int* Container = new int[size];
+	int count = 0;
+	// Reading till end of file
+	while (!InFile.eof())
+	{
+		int n;
+		// read in the elements from the infile
+		// storing it into an array
+		InFile >> n;
+		Container[count] = n;
+		// increment the counter to count through the data
+		count++;
+	}
+	
+	
+	//if want to view the container holding the data call function
+	//print(Container, sz);
+	//start the timer for the program
+	auto start = chrono::high_resolution_clock::now();
 
-    ifstream InFile;
-    ofstream OutFile;
-    openFiles(InFile, OutFile);// prompt for input output
+	//call the function to sort the data
+	CountingSort(Container, count);
+	//stop the timer after is done
+	auto end = chrono::high_resolution_clock::now();
+	// stop the timer and figure out how long it took
+	// from start to stop
+	auto int_s = chrono::duration_cast<chrono::milliseconds>(end - start);
 
-    // Reading till end of file
-    while (!InFile.eof())
-    {
-        int n;
-        // read in the elements from the infile
-        // storing it into an array
-        InFile >> n;
-        Container[count] = n;
-        // increment the counter to count through the data
-        count++;
-    }
-    // start the timer
-    auto start = chrono::high_resolution_clock::now();
+	OutFile << "\n\nUsing Counting Sort, it took : " << int_s.count() <<
+		" milliseconds to sort Ascending InFile" << "\n\n\n";
 
-    // call the function to do the work on sorting
-    selectionSort(Container, count);
+	// display the sorted array to show that array has been sorted
+	//print(Container, count,OutFile);
+	system("pause");
 
-    //stop the timer after is done
-    auto end = chrono::high_resolution_clock::now();
-    // stop the timer and figure out how long it took
-    // from start to stop
-    auto int_s = chrono::duration_cast<chrono::seconds>(end - start);
-
-    OutFile << "\n\nUsing Selection Sort, it took : " << int_s.count() <<
-        " seconds to sort Ascending InFile" << endl;
-    // display the sorted array to show that array has been sorted
-    display(Container, count, OutFile);
-    system("pause");
-
-    //close the file and close out of program
-    InFile.close();
-    OutFile.close();
-    return 0;
+	//close the file and close out of program
+	InFile.close();
+	OutFile.close();
+	return 0;
 }
